982ecec8cf89febc52be2e83c1b7207a49e6ffee
Paweł Migo <migo.pawel@gmail.com>
2020-01-21 13:29:50 +0100

    poprawka1

--- main.js
+++ main.js
@@ -6,5 +6,8 @@ function dlugosc(text1,text2)
 {
   if(wynik1>wynik2){console.log(text1)}
   else if(wynik1<wynik2){console.log(text2)}
-  else if(wynik1==wynik2){console.log('ilość równa')}   
-} //Napisz funkcję, która przyjmuje stworzone zmienne jako parametry. Funkcja ma zwracać string, który jest dłuższy. Wynik działania funkcji wyświetl w konsoli.
+  else if(wynik1==wynik2){console.log('ilość równa')}
+  return (wynik1,wynik2);
+}
+
+//Napisz funkcję, która przyjmuje stworzone zmienne jako parametry. Funkcja ma zwracać string, który jest dłuższy. Wynik działania funkcji wyświetl w konsoli.
