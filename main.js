ae2ca462d3535b08a6e180c2a05b8210dd6a8838
Paweł Migo <migo.pawel@gmail.com>
2020-01-24 18:14:18 +0100

    poprawione

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-const text1=("Uwielbiam JavaScript");
+const text1=("Uwielbiam Javascript");
 const wynik1=text1.length;
 const text2=("Jestem świetnym programistą");
 const wynik2=text2.length;//Zdeklaruj dwie zmienne o dowolnych nazwach, do których przypisz kolejne stringi
-function dlugosc(text1,text2)
+function dlugosc()
 {
-  if(wynik1>wynik2){console.log(text1)}
-  else if(wynik1<wynik2){console.log(text2)}
-  else if(wynik1==wynik2){console.log('ilość równa')}
-  return (wynik1,wynik2);
+  if(wynik1>wynik2){
+    return(text1)}
+  else if(wynik1<wynik2){
+    return(text2)}
+  else if(wynik1==wynik2){
+    return('ilość równa')}
+  console.log(wynik1,wynik2);
 }
+dlugosc();
 
 //Napisz funkcję, która przyjmuje stworzone zmienne jako parametry. Funkcja ma zwracać string, który jest dłuższy. Wynik działania funkcji wyświetl w konsoli.
